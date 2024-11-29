f1a277d7bbc5edfe982aa6b1fbab6599bf936806
lizwithcoffee <luizewaraujo@gmail.com>
2024-11-29 12:58:45 +0000

    aula5_exercicios.py

--- python/aula4_exercicios.py
+++ python/aula4_exercicios.py
@@ -7,7 +7,7 @@ if idade >= 18:
  print("Você é maior de idade.")
 
 elif idade < 18 and idade > 12:
- print("Você é adolescente.")Gr
+ print("Você é adolescente.")
 
 else:
  print("Você é criança.")
